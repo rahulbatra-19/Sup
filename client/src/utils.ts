c4c8cd326188eba3c9a22e518d83482143292c4f
rahulbatra-19 <batrarahul409@gmail.com>
2024-03-21 00:33:27 +0530

    some imports

--- client/src/utils.ts
+++ client/src/utils.ts
@@ -3,15 +3,6 @@ import { BASE_URL } from "./config";
 
 const axios = axios_.create({
   baseURL: BASE_URL,
-  //   headers: {
-  //     // "Content-Type": "application/json",
-  //     "x-auth-token": localStorage.getItem("x-auth-token"),
-  //     // lowercase because headers are automtically turned to lower case
-  //     workspaceid:
-  //       localStorage.getItem("workspaceid") ||
-  //       JSON.parse(localStorage.getItem("owner") || "{}")?.workSpaces?.[0]
-  //         ?.workSpaceId,
-  //   },
 });
 
 export default axios;
