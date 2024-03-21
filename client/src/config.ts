a3b4da7053c08a330881fca6ae6198619793ef63
rahulbatra-19 <batrarahul409@gmail.com>
2024-03-22 01:41:11 +0530

    added backend url

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,2 +1,2 @@
-export const BASE_URL = "http://localhost:3001";
-// export const BASE_URL = "https://sup-12pm.onrender.com";
+// export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = "https://sup-12pm.onrender.com";
